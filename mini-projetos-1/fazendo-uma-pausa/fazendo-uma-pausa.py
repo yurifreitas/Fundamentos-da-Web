8c88ae71a86699a3d426f2566e2c9eec9f5fff74
yuribzfreitas <yuribzfreitas@gmail.com>
2018-07-29 19:16:29 -0300

    fazendo-uma-pausa
    definir um intervelo

--- mini-projetos-1/fazendo-uma-pausa/fazendo-uma-pausa.py
+++ mini-projetos-1/fazendo-uma-pausa/fazendo-uma-pausa.py
@@ -1,3 +1,14 @@
 import webbrowser
+import time
 
-webbrowser.open("https://www.youtube.com/watch?v=uUHHdNa81NM")
+paradas = int(input("Digite o numero de de intervalos de 15 min para seu foco? "))
+contador = 0
+
+while(contador < paradas) :
+    time.sleep(15*60)
+    print ("É hora de fazer uma pausa de 5 min")
+    webbrowser.open("https://www.youtube.com/watch?v=uUHHdNa81NM")
+    time.sleep(5*60)
+    print ("É hora de voltar ao trabalho")
+    time.sleep(10)
+    contador =contador + 1
